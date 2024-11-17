a76d34f3a85bf5d32cdcdb5186f3ee381a0336dd
Mijia-t <mtao5@jh.edu>
2024-11-17 14:46:58 -0500

    Update final_version.py

--- final_version.py
+++ final_version.py
@@ -176,3 +176,67 @@ class Block:
 
         '''
         return f"Block(type={self.block_type}, position={self.position})"
+
+class GridImage:
+    def __init__(self, grid, lasers, points, file_path):
+        '''
+        Sets up the GridImage.
+        
+        Parameters
+        ----------
+        grid : list of str
+            A 2D list shows the grid layout
+        lasers : list of tuple
+            List of laser start points
+        points : list of tuple
+            List of target points
+        file_path : str
+            The path of saving image
+
+        Returns
+        -------
+        None.
+
+        '''
+        self.grid = grid
+        self.lasers = lasers
+        self.points = points
+        self.file_path = file_path
+
+        def build_image(self):
+        '''
+        Creates and saves an image of the grid, lasers, and points.
+
+        Returns
+        -------
+        None.
+
+        '''
+        
+        # Creat the image
+        img = Image.new(mode="RGB", size=(100, 100))
+        
+        # Draw the grid and color the cells, black as 'x' , white as 'o'
+        x = 0
+        for i in self.grid:
+            y = 0
+            string = i.split(' ')
+            for j in string:
+                color = (0, 0, 0) if j == 'x' else (255, 255, 255)
+                img.putpixel((10 * x, 10 * y), color)
+                y += 1
+            x += 1
+            
+        # Draw the laser and color it as red
+        for laser in self.lasers:
+            x, y = laser[:2]
+            img.putpixel((10 * x, 10 * y), (255, 0, 0))
+            
+        # Draw the target points and color it as green
+        for x, y in self.points:
+            img.putpixel((10 * x, 10 * y), (0, 255, 0))
+            
+        # Save the updated image
+        img.save(self.file_path + '.png')
+
+
